94b9b41bfee282c6e93d1665ee4cbbedf30173c9
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-08-12 14:53:12 +0200

    :test_tube: Add first test

--- test/models/power_inverter_production_test.rb
+++ test/models/power_inverter_production_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class PowerInverterProductionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save power_inverter_production without identifier" do
+    power_inverter_production = PowerInverterProduction.new
+    assert_not power_inverter_production.save
+  end
 end
