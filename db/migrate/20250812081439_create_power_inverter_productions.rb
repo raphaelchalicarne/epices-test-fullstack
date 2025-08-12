b5cd7c7751c1e0f54251ac03353c88240fa9c82f
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-08-12 10:34:15 +0200

    :card_file_box: Add PowerInverterProductions columns

--- db/migrate/20250812081439_create_power_inverter_productions.rb
+++ db/migrate/20250812081439_create_power_inverter_productions.rb
@@ -1,6 +1,10 @@
 class CreatePowerInverterProductions < ActiveRecord::Migration[8.0]
   def change
     create_table :power_inverter_productions do |t|
+      t.integer :identifier
+      t.datetime :datetime
+      t.integer :energy
+
       t.timestamps
     end
   end
