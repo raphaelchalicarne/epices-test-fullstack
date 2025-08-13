98b7443c04cf2b4c40a15c4aa7b86b3a6dd8ed09
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-08-13 17:09:49 +0200

    :test_tube: Trying to test CSV upload

--- test/controllers/import_productions_controller_test.rb
+++ test/controllers/import_productions_controller_test.rb
@@ -3,5 +3,20 @@ class ImportProductionsControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test "should create PowerInverterProduction" do
+    assert_difference("PowerInverterProduction.count") do
+      # Mocking an input CSV file
+      header = "identifier,datetime,energy"
+      row2 = "1,11/07/25 06:00,194"
+      row3 = "2,11/07/25 06:00,458"
+      rows = [ header, row2, row3 ]
+      test_csv_file = CSV.open("tmp/test.csv", "w") do |csv|
+        rows.each do |row|
+          csv << row.split(",")
+        end
+      end
+      post import_url, params: { production_file: test_csv_file }
+    end
+  end
 end
-  
