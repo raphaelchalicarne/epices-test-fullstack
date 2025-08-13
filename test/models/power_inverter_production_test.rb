c0fa399581072bbe9916bd2f0151d36c70a93d4d
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-08-13 10:00:18 +0200

    :test_tube: Test attributes presence

--- test/models/power_inverter_production_test.rb
+++ test/models/power_inverter_production_test.rb
@@ -2,7 +2,26 @@ require "test_helper"
 
 class PowerInverterProductionTest < ActiveSupport::TestCase
   test "should not save power_inverter_production without identifier" do
-    power_inverter_production = PowerInverterProduction.new
+    power_inverter_production = PowerInverterProduction.new(
+      datetime: DateTime.strptime('10/07/25 06:00', '%d/%m/%y %H:%M'),
+      energy: 343
+      )
+    assert_not power_inverter_production.save
+  end
+
+  test "should not save power_inverter_production without datetime" do
+    power_inverter_production = PowerInverterProduction.new(
+      identifier: 1,
+      energy: 343
+      )
+    assert_not power_inverter_production.save
+  end
+
+  test "should not save power_inverter_production without energy" do
+    power_inverter_production = PowerInverterProduction.new(
+      identifier: 1,
+      datetime: DateTime.strptime('10/07/25 06:00', '%d/%m/%y %H:%M')
+      )
     assert_not power_inverter_production.save
   end
 end
