5c68b0168eaddf6a6c7d72a3304616e3e00c0bd2
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-08-13 16:28:30 +0200

    :sparkles: Parse CSV data

--- app/controllers/import_productions_controller.rb
+++ app/controllers/import_productions_controller.rb
@@ -5,6 +5,15 @@ class ImportProductionsController < ApplicationController
   def import
     uploaded_file = params[:production_file]
     Rails.logger.info uploaded_file
-    render uploaded_file
+    if uploaded_file.present?
+      csv_data = CSV.parse(uploaded_file.read, headers: true)
+      csv_data.each do |row|
+        PowerInverterProduction.create(
+          identifier: row["identifier"],
+          datetime: DateTime.strptime(row["datetime"], "%d/%m/%y %H:%M"),
+          energy: row["energy"]
+          )
+      end
+    end
   end
 end
