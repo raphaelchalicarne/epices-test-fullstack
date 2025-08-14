abfd9b6b500c16491c6cc819cf03d9dcf80f8e46
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-08-14 16:26:01 +0200

    :recycle: Refactor flash and render

--- app/controllers/import_productions_controller.rb
+++ app/controllers/import_productions_controller.rb
@@ -9,23 +9,20 @@ class ImportProductionsController < ApplicationController
       uploaded_file.rewind
     else
       error_message = "The file sent is invalid"
-      flash.now[:alert] = error_message
-      render plain: error_message, status: :bad_request and return
+      flash_render(error_message, :alert, :bad_request) and return
     end
 
     csv_content = uploaded_file.read
     if csv_content.blank?
       error_message = "Empty CSV file"
-      flash.now[:alert] = error_message
-      render plain: error_message, status: :bad_request and return
+      flash_render(error_message, :alert, :bad_request) and return
     end
 
     begin
       csv_data = CSV.parse(csv_content, headers: true)
     rescue CSV::MalformedCSVError => e
       error_message = "CSV parsing error : #{e.message}"
-      flash.now[:alert] = error_message
-      render plain: error_message, status: :unprocessable_entity and return
+      flash_render(error_message, :alert, :unprocessable_entity)
     end
 
     required_headers = [ "identifier", "datetime", "energy" ]
@@ -33,8 +30,7 @@ class ImportProductionsController < ApplicationController
     missing_headers = required_headers - csv_headers
     unless missing_headers.empty?
       error_message = "Missing headers : #{missing_headers.join(', ')}"
-      flash.now[:alert] = error_message
-      render plain: error_message, status: :unprocessable_entity and return
+      flash_render(error_message, :alert, :unprocessable_entity) and return
     end
 
     begin
@@ -57,12 +53,14 @@ class ImportProductionsController < ApplicationController
       end
     rescue => e
       error_message = "CSV import error : #{e.message}"
-      flash.now[:alert] = error_message
-      render plain: error_message, status: :unprocessable_entity and return
+      flash_render(error_message, :alert, :unprocessable_entity) and return
     end
 
-    notice_message = "Successfully imported the CSV file."
-    flash.now[:notice] = notice_message
-    render plain: notice_message, status: :created and return
+    flash_render("Successfully imported the CSV file.", :notice, :created)
+  end
+
+  def flash_render(message, flash_type, status)
+    flash.now[flash_type] = message
+    render plain: message, status: status
   end
 end
