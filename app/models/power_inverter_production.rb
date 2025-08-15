59e3265c3c3f7c8d37ce53653c4e6b219eb3c957
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-08-15 10:23:46 +0200

    :sparkles: Display Power data

--- app/models/power_inverter_production.rb
+++ app/models/power_inverter_production.rb
@@ -2,4 +2,14 @@ class PowerInverterProduction < ApplicationRecord
     validates :identifier, presence: true
     validates :datetime, presence: true
     validates :energy, presence: true
+
+    def self.identifiers
+        distinct.pluck(:identifier)
+    end
+
+    def self.production(identifier)
+        where(identifier: identifier)
+        .group_by_hour(:datetime)
+        .sum(:energy)
+    end
 end
