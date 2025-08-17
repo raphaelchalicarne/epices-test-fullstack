fc6b06a8fcf3678b435e764a771ca68a55b55204
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-08-17 14:15:47 +0200

    :truck: Rename variables in InverterTest

--- test/models/inverter_test.rb
+++ test/models/inverter_test.rb
@@ -19,19 +19,19 @@ class InverterTest < ActiveSupport::TestCase
   end
 
   test "#hourly_production should return the hourly energy production for a given Inverter" do
-    expected_hourly_production_one_10_7 = {
+    expected_hourly_production_one_10 = {
       DateTime.civil(2025, 7, 10, 6, 0, 0, 0).in_time_zone => 343,
       DateTime.civil(2025, 7, 10, 7, 0, 0, 0).in_time_zone => 2174
     }
-    expected_hourly_production_one_11_7 = {
+    expected_hourly_production_one_11 = {
       DateTime.civil(2025, 7, 11, 6, 0, 0, 0).in_time_zone => 343
     }
-    expected_hourly_production_two_10_7 = {
+    expected_hourly_production_two_10 = {
       DateTime.civil(2025, 7, 10, 6, 0, 0, 0).in_time_zone => 4494
     }
-    assert_equal Inverter.find_by(id: 1).hourly_production(Date.civil(2025, 7, 10)), expected_hourly_production_one_10_7
-    assert_equal Inverter.find_by(id: 1).hourly_production(Date.civil(2025, 7, 11)), expected_hourly_production_one_11_7
-    assert_equal Inverter.find_by(id: 2).hourly_production(Date.civil(2025, 7, 10)), expected_hourly_production_two_10_7
+    assert_equal Inverter.find_by(id: 1).hourly_production(Date.civil(2025, 7, 10)), expected_hourly_production_one_10
+    assert_equal Inverter.find_by(id: 1).hourly_production(Date.civil(2025, 7, 11)), expected_hourly_production_one_11
+    assert_equal Inverter.find_by(id: 2).hourly_production(Date.civil(2025, 7, 10)), expected_hourly_production_two_10
   end
 
   test "#daily_production should return the total energy production for a given Inverter and a given day" do
